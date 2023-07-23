a60c39f843b2126d6e0142091674de66a8a0bbf0
Espinosa Nicolas <34327988+theallison1@users.noreply.github.com>
2023-07-23 20:01:26 -0300

    Update Dashboard.js

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,31 +1,16 @@
 import React from 'react'
 import Background from '../components/Background'
 import Logo from '../components/Logo'
-
 import Paragraph from '../components/Paragraph'
-
-
 import { Button, Text } from 'react-native';
 import { Drawer } from 'react-native-drawer-layout';
 
 
 export default function Dashboard({ navigation }) {
+   
   return (
     <Background>
-       
-       <Drawer
-      open={open}
-      onOpen={() => setOpen(true)}
-      onClose={() => setOpen(false)}
-      renderDrawerContent={() => {
-        return <Text>Drawer content</Text>;
-      }}
-    >
-      <Button
-        onPress={() => setOpen((prevOpen) => !prevOpen)}
-        title={`${open ? 'Close' : 'Open'} drawer`}
-      />
-    </Drawer>
+     
     </Background>
   )
 }
