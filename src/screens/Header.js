81d097feb32171e5d7c90ce8a9d2b370dc2f42e4
Espinosa Nicolas <34327988+theallison1@users.noreply.github.com>
2023-07-23 20:04:55 -0300

    Update Header.js

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -4,54 +4,7 @@ import ResponsiveMenu from "react-responsive-navbar";
 
 export default function(props) {
   return (
-    <header className="header">
-      <div className="wrap">
-        <header className="logo">
-          <h1 className="logo-title">
-            <a href="#" className="logo-link">
-              Video Bucket
-            </a>
-          </h1>
-        </header>
-        <nav className="menu">
-          <ResponsiveMenu
-            menuOpenButton={
-              <div className="menu hamburger-menu menu-btn">
-              </div>
-            }
-            menuCloseButton={
-              <div className="menu hamburger-menu menu-btn">
-                <Close size={32} />
-              </div>
-            }
-            changeMenuOn="900px"
-            menu={
-              <ul className="menu-list">
-                <li className="menu-item is-active menu-item--play">
-                  <a href="#" className="menu-link">
-                    Navigation A
-                  </a>
-                </li>
-                <li className="menu-item is-active menu-item--play">
-                  <a href="#" className="menu-link">
-                    Navigation B
-                  </a>
-                </li>
-                <li className="menu-item is-active menu-item--play">
-                  <a href="#" className="menu-link">
-                    Navigation C
-                  </a>
-                </li>
-                <li className="menu-item is-active menu-item--play">
-                  <a href="#" className="menu-link">
-                    Navigation D
-                  </a>
-                </li>
-              </ul>
-            }
-          />
-        </nav>
-      </div>
-    </header>
+   
+    
   );
 }
