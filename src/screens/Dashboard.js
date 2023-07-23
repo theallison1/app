1af68dbc050739ed65acd09e0570e21599a4bc55
nicolas espinosa <nico.2011.pr@gmail.com>
2023-07-23 17:33:13 -0300

    update  icons

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Background from '../components/Background'
 import Logo from '../components/Logo'
-import Header from '../components/Header'
+
 import Paragraph from '../components/Paragraph'
 import Button from '../components/Button'
-import ResponsiveMenu from 'react-responsive-navbar'
-import Header from '../screens/Header'
+import Header from './Header'
+
+
 
 export default function Dashboard({ navigation }) {
   return (
